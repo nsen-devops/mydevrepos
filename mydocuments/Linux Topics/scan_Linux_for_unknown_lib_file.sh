8af05ad05d880e80a416d5a83e88c812df4c8013
Nitin <nitinsen@gmail.com>
2020-08-30 17:04:15 +0530

    for testing

--- mydocuments/Linux Topics/scan_Linux_for_unknown_lib_file.sh
+++
@@ -1,2 +0,0 @@
-find /lib*/ -iname "*.so*" -type f -exec  rpm -qf {} 2>&1  \; | grep "not owned by any package"|grep -v "vmware-tools"
-find /usr/lib*/ -iname "*.so*" -type f -exec  rpm -qf {} 2>&1  \; | grep "not owned by any package"|grep -v "vmware-tools"
